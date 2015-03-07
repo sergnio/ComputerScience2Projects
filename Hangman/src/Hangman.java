9c5b13061e6f1e3d7087bb8a0c773cd149e25d0b
Sergnio <srn1122@yahoo.com>
2015-03-06 20:50:07 -0600

    Commit #1

--- Hangman/src/Hangman.java
+++ Hangman/src/Hangman.java
@@ -1,3 +1,4 @@
+import java.io.FileNotFoundException;
 import java.util.List;
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -5,8 +6,60 @@ import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
+
 public class Hangman {
 
-	
-	
+    ArrayList<Character> guessedLetters = new ArrayList<Character>(
+            chosenWord.length());
+
+
+    public void setChosenLetter(char chosenLetter) {
+        this.chosenLetter = chosenLetter;
+    }
+
+    public void setGuessedLetters(ArrayList<Character> guessedLetters) {
+        this.guessedLetters = guessedLetters;
+    }
+
+    public void readNextChar() {
+        char chosenLetter = readNext.charAt(0);
+    }
+
+    public static void isGuessed() {
+        if (!guessedLetters.contains(chosenLetter)) {
+            guessedLetters.add(chosenLetter);
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            @SuppressWarnings("resource")
+            BufferedReader txtReader = new BufferedReader(new FileReader("src/Resources/Hangman.txt"));
+            String line = txtReader.readLine();
+            ArrayList<String> words = new ArrayList<String>();
+            while (line != null) {
+                String[] wordsLine = line.split(" ");
+                for (String word : wordsLine) {
+                    words.add(word);
+                }
+                line = txtReader.readLine();
+            }
+
+            
+
+            Random random = new Random();
+            String randomWord = words.get(random.nextInt(words.size()));
+
+            StringBuilder chosenWord = new StringBuilder("none");
+            chosenWord = new StringBuilder(randomWord);
+
+        } catch (Exception exception) {
+            System.err.printf("Exception caught: %s", exception.toString());
+            System.exit(0);
+
+        }
+
+
+    }
+
 }
